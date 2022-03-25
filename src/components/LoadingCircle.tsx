941f9433445e22c173eb2b81983568c837f14dab
Mateus Vieira <68292695+MateusVrs@users.noreply.github.com>
2022-03-25 17:14:12 -0300

    LoadingCircle -> component + styles

---
+++ src/components/LoadingCircle.tsx
@@ -0,0 +1,9 @@
+import '../assets/scss/components/loadingcircle.scss'
+
+export function LoadingCircle() {
+    return (
+        <div className="loading-container">
+            <div className="loading-circle"></div>
+        </div>
+    )
+}
