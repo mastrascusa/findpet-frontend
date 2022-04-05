0327e8c38d622f256d4a0bd4476f80684399dc4e
Mateus Vieira <68292695+MateusVrs@users.noreply.github.com>
2022-04-04 23:07:53 -0300

    user state on component links + take out fadein styles

--- src/components/MainHeader.tsx
+++ src/components/MainHeader.tsx
@@ -1,10 +1,14 @@
-import { useState } from 'react'
-import { useLocation } from 'react-router-dom'
+import { Fragment, useState } from 'react'
+import { Link, useLocation } from 'react-router-dom'
+
 import logoImg from '../assets/images/logo.svg'
+
+import { useAuth } from '../hooks/useAuth'
 
 import '../assets/scss/components/mainheader.scss'
 
 export function MainHeader() {
+    const { user } = useAuth()
     const [optionsOpen, setOptionOpen] = useState(false)
     const location = useLocation()
 
@@ -14,8 +18,19 @@ export function MainHeader() {
                 <div className="blur"></div>
                 <div className="options">
                     <div className="links">
-                        <a href={location.pathname === '/' ? '#register' : '#login'} onClick={() => setOptionOpen(!optionsOpen)}>{location.pathname === '/' ? 'Cadastro' : 'Login'}</a>
-                        <a href="#about" onClick={() => setOptionOpen(!optionsOpen)}>Sobre</a>
+                        {user ?
+                            <Fragment>
+                                <Link to='/home'>Pets Perdidos</Link>
+                                <Link to='/publish'>Publicar Pet</Link>
+                                <Link to='/pets'>Meus Pets</Link>
+                                <Link to='/chats'>Conversas</Link>
+                            </Fragment>
+                            :
+                            <Fragment>
+                                <a href={location.pathname === '/' ? '#register' : '#login'} onClick={() => setOptionOpen(!optionsOpen)}>{location.pathname === '/' ? 'Cadastro' : 'Login'}</a>
+                                <a href="#about" onClick={() => setOptionOpen(!optionsOpen)}>Sobre</a>
+                            </Fragment>
+                        }
                     </div>
                 </div>
             </div>
